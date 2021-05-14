fd3eb7a4974171f9b6a53e37bfc36bcf47b3af9e
Antonio Alford-El <antonio.alford@gmail.com>
2021-05-14 13:10:30 -0400

    project 3 help

--- project3/main.js
+++ project3/main.js
@@ -1,6 +1,6 @@
 let button = document.querySelector("#button");
 let reset = document.querySelector("#resetButton");
-let textGuess = document.querySelector("#text");
+let textGuess = document.querySelector("#text").value;
 let msg = document.querySelector("#msg");
 button.addEventListener("click", onSubmit());
 reset.addEventListener("click", start);
@@ -9,18 +9,22 @@ let numberOfGuess = 3;
 let randomNum = (Math.floor(Math.random() * 100) + 1);
 
 function onSubmit() {
-    while (count < numberOfGuess) {
-        if (textGuess == randomNum) {
-            msg.text = "You Guessed Correct!!";
-            break;
-        }
-        else if (textGuess > randomNum) {
-            msg.text = "Too High. Try again.";
-        } else if (textGuess < randomNum) {
-            msg.text = "Too Low Try again."
-        } else {
-            msg.text = "Error!!";
-        }
-        count++;
+
+    if (textGuess == randomNum) {
+        msg.text = "You Guessed Correct!!";
+        break;
     }
+    else if (textGuess > randomNum) {
+        msg.text = "Too High. Try again.";
+        count++
+    } else if (textGuess < randomNum) {
+        msg.text = "Too Low Try again."
+        count++
+    } else if (count == numberOfGuess)
+        break;
+    else {
+        msg.text = "Error!!";
+    }
+
 }
+
