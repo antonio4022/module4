c8f0b9ec28b8bc39f83f00bceb8fad76556e017f
Antonio Alford-El <antonio.alford@gmail.com>
2021-05-29 20:00:06 -0400

    project 3 completion with help

--- project3/main.js
+++ project3/main.js
@@ -7,43 +7,44 @@ let randomNum = (Math.floor(Math.random() * 100) + 1);
 function onSubmit(event) {
     event.preventDefault()
     let textGuess = input.value;
-    if (count == 3) {
-        msg.innerHTML = "Your 3 tries have been used. The number was " + randomNum;
-        input.value = '';
-        button.innerHTML = 'Reset';
-        // if ('reset') {
-        //     count = 0;
-        //     msg.innerHTML = '';
-        //     randomNum = (Math.floor(Math.random() * 100) + 1);
-        // }
-
-
-    } else if
-        (textGuess == '') {
+    if (button.innerHTML == "Reset") {
+        console.log("resetting the game")
+        randomNum = (Math.floor(Math.random() * 100) + 1);
         msg.innerHTML = '';
-        button.innerHTML = 'Submit';
         count = 0;
-    } else if (textGuess == randomNum) {
-        msg.innerHTML = "You Guessed Correct!!";
         button.innerHTML = 'Submit';
+        return
+    }
+    if (textGuess == '') {
+        msg.innerHTML = '';
+    }
+    else if (textGuess == randomNum) {
+        msg.innerHTML = "You Guessed Correct!!";
     }
 
     else if (textGuess > randomNum) {
         msg.innerHTML = "Too High Try again.";
-        button.innerHTML = 'Submit';
-        count++
     }
 
     else if (textGuess < randomNum) {
         msg.innerHTML = "Too Low Try again.";
-        button.innerHTML = 'Submit';
-        count++
     }
 
     else {
         msg.innerHTML = "Error!!";
-        button.innerHTML = 'Submit';
+    }
+
+
+    count++
+
+    console.log("count ", count)
+    if (count >= 3) {
+        console.log('No more tries left, time to reset')
+        msg.innerHTML += " Your 3 tries have been used. The number was " + randomNum;
+        input.value = '';
+        button.innerHTML = 'Reset';
     }
 
 }
+
 
