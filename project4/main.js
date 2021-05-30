a97148f5a71e3e678fbf799ee10d7bc5ebc94c87
Antonio Alford-El <antonio.alford@gmail.com>
2021-05-30 15:22:09 -0400

    project4 help with user selection

--- project4/main.js
+++ project4/main.js
@@ -4,7 +4,6 @@ let playerChoiceScissor = document.getElementById("scissors");
 let msg = document.getElementById("msg");
 let result = document.getElementById("result");
 let yourChoice = document.getElementById('yourChoice');
-possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
 
@@ -34,43 +33,44 @@ function generateComputerChoice(e) {
         userChoice = 'paper';
     }
     if (playerChoiceScissor) {
-        userChoice = 'scissor'; {
+        userChoice = 'scissors';
 
+    }
+
+    getResult()
+    console.log(userChoice);
+    console.log(computerChoice);
+
+    msg.innerHTML = 'The computer choose ' + computerChoice;
+    function getResult(e) {
+        if (computerChoice == userChoice) {
+            result.innerHTML = 'It was a tie. try again'
+            yourChoice.innerHTML = 'you choose ' + userChoice
         }
-        getResult()
-        console.log(userChoice);
-        console.log(computerChoice);
-
-        msg.innerHTML = 'The computer choose ' + computerChoice;
-        function getResult(e) {
-            if (computerChoice == userChoice) {
-                result.innerHTML = 'It was a tie. try again'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'rock' && userChoice == 'paper') {
-                result.innerHTML = 'You win, paper beats Rock!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'rock' && userChoice == 'scissors') {
-                result.innerHTML = 'You lose, rock beats scissors!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'paper' && userChoice == 'scissors') {
-                result.innerHTML = 'You win, scissors beats paper!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'paper' && userChoice == 'rock') {
-                result.innerHTML == 'You lose, paper beats Rock!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'scissors' && userChoice == 'rock') {
-                result.innerHTML = 'You win, rock beats scissors!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
-            if (computerChoice == 'scissors' && userChoice == 'paper') {
-                result.innerHTML = 'You lose, scissors beats papar!'
-                yourChoice.innerHTML = 'you choose ' + userChoice
-            }
+        if (computerChoice == 'rock' && userChoice == 'paper') {
+            result.innerHTML = 'You win, paper beats Rock!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
+        }
+        if (computerChoice == 'rock' && userChoice == 'scissors') {
+            result.innerHTML = 'You lose, rock beats scissors!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
+        }
+        if (computerChoice == 'paper' && userChoice == 'scissors') {
+            result.innerHTML = 'You win, scissors beats paper!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
+        }
+        if (computerChoice == 'paper' && userChoice == 'rock') {
+            result.innerHTML == 'You lose, paper beats Rock!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
+        }
+        if (computerChoice == 'scissors' && userChoice == 'rock') {
+            result.innerHTML = 'You win, rock beats scissors!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
+        }
+        if (computerChoice == 'scissors' && userChoice == 'paper') {
+            result.innerHTML = 'You lose, scissors beats papar!'
+            yourChoice.innerHTML = 'you choose ' + userChoice
         }
     }
+
 }
