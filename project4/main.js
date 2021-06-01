a779bec3fb19b9577e7ae9c2725ca8f1edbe39a6
Antonio Alford-El <antonio.alford@gmail.com>
2021-06-01 11:33:07 -0400

    project 4 cleanup

--- project4/main.js
+++ project4/main.js
@@ -4,20 +4,18 @@ let playerChoiceScissor = document.getElementById("scissors");
 let msg = document.getElementById("msg");
 let result = document.getElementById("result");
 let yourChoice = document.getElementById('yourChoice');
-let userChoice
+
 let computerChoice
 
 rock.addEventListener("click", generateComputerChoice);
 paper.addEventListener("click", generateComputerChoice);
 scissors.addEventListener("click", generateComputerChoice);
-playerChoiceRock.addEventListener("click", rockResult);
-playerChoicePaper.addEventListener("click", paperResult);
-playerChoiceScissor.addEventListener("click", scissorResult);
 
 
 
 
-function generateComputerChoice(e) {
+
+function generateComputerChoice(event) {
 
     let randomNum = Math.floor(Math.random() * 3) + 1;
 
@@ -35,8 +33,9 @@ function generateComputerChoice(e) {
 }
 
 
-function rockResult(e) {
-    userChoice = 'rock';
+function detail(event) {
+    var userChoice = event.target.id
+    console.log(userChoice);
     if (computerChoice == userChoice) {
         result.innerHTML = 'It was a tie. try again'
         yourChoice.innerHTML = 'you choose ' + userChoice
@@ -66,68 +65,3 @@ function rockResult(e) {
         yourChoice.innerHTML = 'you choose ' + userChoice
     }
 }
-
-function paperResult(e) {
-    userChoice = 'paper';
-    if (computerChoice == userChoice) {
-        result.innerHTML = 'It was a tie. try again'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'rock' && userChoice == 'paper') {
-        result.innerHTML = 'You win, paper beats Rock!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'rock' && userChoice == 'scissors') {
-        result.innerHTML = 'You lose, rock beats scissors!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'paper' && userChoice == 'scissors') {
-        result.innerHTML = 'You win, scissors beats paper!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'paper' && userChoice == 'rock') {
-        result.innerHTML == 'You lose, paper beats Rock!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'scissors' && userChoice == 'rock') {
-        result.innerHTML = 'You win, rock beats scissors!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'scissors' && userChoice == 'paper') {
-        result.innerHTML = 'You lose, scissors beats papar!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-}
-
-function scissorResult(e) {
-    userChoice = 'scissors';
-    if (computerChoice == userChoice) {
-        result.innerHTML = 'It was a tie. try again'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'rock' && userChoice == 'paper') {
-        result.innerHTML = 'You win, paper beats Rock!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'rock' && userChoice == 'scissors') {
-        result.innerHTML = 'You lose, rock beats scissors!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'paper' && userChoice == 'scissors') {
-        result.innerHTML = 'You win, scissors beats paper!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'paper' && userChoice == 'rock') {
-        result.innerHTML == 'You lose, paper beats Rock!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'scissors' && userChoice == 'rock') {
-        result.innerHTML = 'You win, rock beats scissors!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-    if (computerChoice == 'scissors' && userChoice == 'paper') {
-        result.innerHTML = 'You lose, scissors beats papar!'
-        yourChoice.innerHTML = 'you choose ' + userChoice
-    }
-}
-
