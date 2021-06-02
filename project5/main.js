b1d2ed05336af25e52a56f403d56014e2d728742
Antonio Alford-El <antonio.alford@gmail.com>
2021-06-02 13:33:17 -0400

    project 5 submittal for review

--- project5/main.js
+++ project5/main.js
@@ -1,7 +1,7 @@
 let sec = 0
 let min = 0
 let hour = 0
-let meridian = "A.M.";
+let meridian = "  A.M.";
 
 function theTime() {
     let date = new Date();
@@ -10,14 +10,19 @@ function theTime() {
     let s = date.getSeconds();
 
     if (h > 12) {
-        meridian = P.M.
-            h - 12
+        meridian = "  P.M."
     }
     if (m < 10) {
-        m = 0 + m;
+        m = "0" + m;
     }
     if (s < 10) {
-        s = 0 + s;
+        s = "0" + s;
+    }
+    if (h > 12) {
+        h = h - 12;
+    }
+    if (h == 0) {
+        h = 1;
     }
     var time = h + ":" + m + ":" + s + meridian;
     document.getElementById("clock").innerHTML = time;
