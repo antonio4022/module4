dc9cc001f762709958ed24758d2ec8e64e645abe
Antonio Alford-El <antonio.alford@gmail.com>
2021-06-12 19:30:23 -0400

    project6 corrections

--- project6/main.js
+++ project6/main.js
@@ -1,33 +1,40 @@
 let check = document.getElementById('check');
 
-check.addEventListener(click, answerCheck);
+//check.addEventListener("click", answerCheck);
 
 
 
 function answerCheck() {
-    var c = 0;
-    var q1 = document.quiz.question1.innerHTML;
-    var q2 = document.quiz.question2.innerHTML;
-    var q3 = document.quiz.question3.innerHTML;
-    var q4 = document.quiz.question4.innerHTML;
-    var result = document.getElementById('result');
-    if (q1 == "America") {
-        c++;
-        document.write(document.value)
-    }
-    if (q2 == "Russia") {
-        c++;
-        document.write(document.value)
-    }
-    if (q3 == "Florida") {
-        c++;
-        document.write(document.value)
-    }
-    if (q4 == "to keep slavery") {
-        c++;
-        document.write(document.value)
-    }
-    document.write(c);
+    var ele1 = getElementsByName("question1");
+    var ele2 = getElementsByName("question2");
+    var ele3 = getElementsByName("question3");
+    var ele4 = getElementsByName("question4");
+    var result1 = getElementById("answer1");
+    var result2 = getElementById("answer2");
+    var result3 = getElementById("answer3");
+    var result4 = getElementById("answer4");
 
-    console.log(q1);
+    console.log("hi");
+    for (i = 0; i < ele1.length; i++) {
+        if (ele1[i].checked)
+            document.getElementById("result1").innerHTML
+                = "The correct answer is  " + ele1[i].value;
+    }
+
+    for (i = 0; i < ele2.length; i++) {
+        if (ele2[i].checked)
+            document.getElementById("result2").innerHTML
+                = "The correct answer is  " + ele2[i].value;
+    }
+    for (i = 0; i < ele3.length; i++) {
+        if (ele3[i].checked)
+            document.getElementById("result3").innerHTML
+                = "The correct answer is  " + ele3[i].value;
+    }
+    for (i = 0; i < ele4.length; i++) {
+        if (ele4[i].checked)
+            document.getElementById("result4").innerHTML
+                = "The correct answer is " + ele4[i].value;
+    }
+
 }
