df8b11a064d4e8cd5e90f93e6bf6096cc77610b0
Antonio Alford-El <antonio.alford@gmail.com>
2021-06-12 20:29:16 -0400

    project 6 correction

--- project6/main.js
+++ project6/main.js
@@ -5,35 +5,35 @@ let check = document.getElementById('check');
 
 
 function answerCheck() {
-    var ele1 = getElementsByName("question1");
-    var ele2 = getElementsByName("question2");
-    var ele3 = getElementsByName("question3");
-    var ele4 = getElementsByName("question4");
-    var result1 = getElementById("answer1");
-    var result2 = getElementById("answer2");
-    var result3 = getElementById("answer3");
-    var result4 = getElementById("answer4");
+    var ele1 = document.getElementsByName("question1");
+    var ele2 = document.getElementsByName("question2");
+    var ele3 = document.getElementsByName("question3");
+    var ele4 = document.getElementsByName("question4");
+    var result1 = document.getElementById("answer1");
+    var result2 = document.getElementById("answer2");
+    var result3 = document.getElementById("answer3");
+    var result4 = document.getElementById("answer4");
 
     console.log("hi");
     for (i = 0; i < ele1.length; i++) {
         if (ele1[i].checked)
-            document.getElementById("result1").innerHTML
+            return document.getElementById("result1").innerHTML
                 = "The correct answer is  " + ele1[i].value;
     }
 
     for (i = 0; i < ele2.length; i++) {
         if (ele2[i].checked)
-            document.getElementById("result2").innerHTML
+            return document.getElementById("result2").innerHTML
                 = "The correct answer is  " + ele2[i].value;
     }
     for (i = 0; i < ele3.length; i++) {
         if (ele3[i].checked)
-            document.getElementById("result3").innerHTML
+            return document.getElementById("result3").innerHTML
                 = "The correct answer is  " + ele3[i].value;
     }
     for (i = 0; i < ele4.length; i++) {
         if (ele4[i].checked)
-            document.getElementById("result4").innerHTML
+            return document.getElementById("result4").innerHTML
                 = "The correct answer is " + ele4[i].value;
     }
 
