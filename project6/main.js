422d412f5a92b332bdac0795f4950b5280c64fc7
Antonio Alford-El <antonio.alford@gmail.com>
2021-06-15 08:34:10 -0400

    project 6 has 1 issue thats not neccesary to have

--- project6/main.js
+++ project6/main.js
@@ -9,30 +9,63 @@ function answerCheck() {
     var ele2 = document.getElementsByName("question2");
     var ele3 = document.getElementsByName("question3");
     var ele4 = document.getElementsByName("question4");
-    var result1 = document.getElementById("answer1");
-    var result2 = document.getElementById("answer2");
-    var result3 = document.getElementById("answer3");
-    var result4 = document.getElementById("answer4");
-    var correct = document.getElementById('correct');
+    ele1.checked = false;
+    ele2.checked = false;
+    ele3.checked = false;
+    ele4.checked = false;
+    let c = 0;
 
+
+    if (ele1.checked === true) {
+        c++
+    }
+    if (ele2.checked === true) {
+        c++
+    }
+    if (ele3.checked === true) {
+        c++
+    }
+    if (ele4.checked === true) {
+        c++
+    }
 
 
     for (i = 0; i < ele1.length; i++) {
-        if (ele1[i].checked == correct)
-            "result1".innerHTML = "The correct answer is  " + ele1[i].value;
+        if (ele1[3].checked === true) {
+            document.getElementById('answer1').innerHTML = "That is correct. (Operation paperclip)";
+
+
+        } else {
+            document.getElementById('answer1a').innerHTML = "That is incorrect.please try again";
+        }
+
     }
 
     for (i = 0; i < ele2.length; i++) {
-        if (ele2[i].checked == correct)
-            "result2".innerHTML = "The correct answer is  " + ele2[i].value;
+        if (ele2[2].checked === true) {
+            document.getElementById('answer2').innerHTML = "That is correct. more than 20 million Russian lives lost.";
+
+        } else {
+            document.getElementById('answer2a').innerHTML = "That is incorrect.please try again";
+        }
     }
     for (i = 0; i < ele3.length; i++) {
-        if (ele3[i].checked == correct)
-            "result3".innerHTML = "The correct answer is  " + ele3[i].value;
+        if (ele3[2].checked === true) {
+            document.getElementById('answer3').innerHTML = "That is correct. (Operation Northwoods)";
+
+        } else {
+            document.getElementById('answer3a').innerHTML = "That is incorrect.please try again";
+        }
+
     }
     for (i = 0; i < ele4.length; i++) {
-        if (ele4[i].checked == correct)
-            "result4".innerHTML = "The correct answer is " + ele4[i].value;
+        if (ele4[1].checked === true) {
+            document.getElementById('answer4').innerHTML = "That is correct. (The Missouri Comprimise.)";
+
+        } else {
+            document.getElementById('answer4a').innerHTML = "That is incorrect.please try again";
+        }
     }
 
+    return document.getElementById('correct').innerHTML = "You have " + c + " correct."
 }
